d2fef21e440f0124afa54229f281b5a459dbfea9
hungne2k3 <104420895+hungne2k3@users.noreply.github.com>
2024-03-25 06:44:50 -0700

    cắt ghép giao diện admin, xử lý logic phần Login

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 ## Login Admin
 
--   Tao MK cho admin
--   `php artisan tinker`: để tạo 1 token bảo mật cho mật khẩu
--   Sau khi gõ câu lệnh trên thì điền: `echo bcrypt('123456') ` => `$2y$10$Xo3gphzLWYb7zQX12kH/fO69VtBKdMdaOu5p9cLJaFxdNCuQ2VhP6` copy đoạn mẫ trên rồi dán vào bảng `Users` trên `phpMyAdmin` phần `password`
+-   Tao mã hóa mật khẩu cho admin -> lên WEB mã hóa password trong laravel rồi mã hóa mk copy rồi dán vào csdl trên phpMyAdmin
--- app/Http/Controllers/Admin/Users/LoginController.php
+++ app/Http/Controllers/Admin/Users/LoginController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Admin\Users;
 
+use Illuminate\Support\Facades\Session;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class LoginController extends Controller
 {
@@ -12,5 +15,38 @@ class LoginController extends Controller
         $title = 'Đăng Nhập Hệ Thống';
 
         return view('admin/users/login', compact('title'));
+    }
+
+    public function store(Request $request)
+    {
+
+        $rule = [
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ];
+
+        $message = [
+            'email.required' => 'Email bắt buộc nhập',
+            'email.email' => 'Email không đúng định dạng',
+            'password.required' => 'Vui lòng nhập mật khẩu.',
+            'password.min' => 'Mật khẩu phải có ít nhất :min ký tự.',
+        ];
+
+        $request->validate($rule, $message);
+
+        // Kiểm tra đã đăng nhập hay chưa và lưu đăng nhập
+        if (
+            Auth::attempt([
+                'email' => $request->input('email'),
+                'password' => $request->input('password'),
+            ], $request->input('remember'))
+        ) {
+            return redirect()->route('admin');
+        }
+
+        Session::flash('error', 'Email hoặc password không chính sác');
+
+        return redirect()->back();
+        ;
     }
 }
