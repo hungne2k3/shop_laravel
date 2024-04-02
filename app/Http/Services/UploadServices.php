41506e8bb5d8c6746ff2bb8ac69121507f77e4bd
hungne2k3 <104420895+hungne2k3@users.noreply.github.com>
2024-04-02 06:55:20 -0700

    Tạo sản phẩm

--- app/Http/Services/UploadServices.php
+++ app/Http/Services/UploadServices.php
@@ -5,26 +5,6 @@ namespace App\Http\Services;
 
 class UploadService
 {
-    // public function store($request)
-    // {
-    //     // trước khi upload file kiểm tra xem file có tồn tại không
-    //     if ($request->hasFile('file')) {
-    //         try {
-    //             $name = $request->file('file')->getClientOriginalName();
-    //             $pathFull = 'uploads/' . date("Y/m/d");
-
-    //             $request->file('file')->storeAs(
-    //                 'public/' . $pathFull,
-    //                 $name
-    //             );
-
-    //             return '/storage/' . $pathFull . '/' . $name;
-    //         } catch (\Exception $error) {
-    //             return false;
-    //         }
-    //     }
-    // }
-
     public function store($request)
     {
         // trước khi upload file kiểm tra xem file có tồn tại không
